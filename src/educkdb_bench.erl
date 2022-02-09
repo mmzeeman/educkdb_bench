0d373494798954feb628ed0048ac0eb56e14fa01
Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
2022-02-09 23:17:38 +0100

    Use newest educkdb

--- src/educkdb_bench.erl
+++ src/educkdb_bench.erl
@@ -23,37 +23,48 @@ ets() ->
 
 main() ->
     {ok, Db} = educkdb:open(":memory:"),
-    {ok, Conn} = esqlite3:connect(Db),
+    {ok, Conn} = educkdb:connect(Db),
 
-    [] = esqlite3:q("create table bench (a int, b int, c int, d int, e int, f int, g int, h int, i int, j int)", Conn),
+    {ok, _, []} = q(Conn, "create table bench (a int, b int, c int, d int, e int, f int, g int, h int, i int, j int)"),
 
     Values = lists:seq(1, 10),
     Range = lists:seq(1, 100_000),
 
     %% Insert 1_000_000 records
     {Time, _} = timer:tc(fun() ->
-                                 ok = educkdb:query(Conn, "begin;"),
+                                 {ok, [], []} = q(Conn, "begin;"),
 
-                                 {ok, Stmt} = educkdb:prepare("insert into bench values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", Conn),
+                                 {ok, Stmt} = educkdb:prepare(Conn, "insert into bench values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"),
                                  lists:foreach(fun(_) ->
-                                                       [ ok = educkdb:bind_integer(Conn, V, V) || V <- Values ],
-                                                       ok = educkdb_prepare:bind_integer(Stmt, Values),
-                                                       ok = educkdb:execute_prepared(Stmt)
+                                                       [ ok = educkdb:bind_int32(Stmt, V, V) || V <- Values ],
+                                                       {ok, _} = educkdb:execute_prepared(Stmt)
                                                end,
                                                Range),
 
-                                 ok = educkdb:query(Conn, "commit;")
+                                 {ok, [], []} = q(Conn, "commit;")
                          end),
 
     io:fwrite("100_000 inserts took: ~p milliseconds~n", [Time/1000]),
 
     %% Select 100_000 
-    {SelectTime, Rows} = timer:tc(fun() ->
-                                          educkdb:query(Conn, "select * from bench;")
-                                  end),
+    {SelectTime, {ok, _, Rows}} = timer:tc(fun() ->
+                                                   q(Conn, "select * from bench;")
+                                           end),
     
     io:fwrite("Select of ~p rows took: ~p milliseconds~n", [length(Rows), SelectTime/1000]),
 
     ok.
 
+
+q(Conn, Query) ->
+    case educkdb:query(Conn, Query) of
+        {ok, Result} -> educkdb:extract_result(Result);
+        {error, _}=E -> E
+    end.
+
+x(Stmt) ->
+    case educkdb:execute_prepared(Stmt) of
+        {ok, Result} -> educkdb:extract_result(Result);
+        {error, _}=E -> E
+    end.
 
