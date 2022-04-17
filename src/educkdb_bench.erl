9ab2573acd87581378ad966afc38a5141ac1aff1
Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
2022-04-17 20:35:48 +0200

    Use new extract api

--- src/educkdb_bench.erl
+++ src/educkdb_bench.erl
@@ -18,20 +18,27 @@ ets() ->
 
     io:fwrite("100_000 inserts took: ~p milliseconds~n", [Time/1000]),
 
+    %% Select 100_000 
+    {SelectTime, Rows} = timer:tc(fun() ->
+                                          ets:tab2list(Ref)
+                                  end),
+
+    io:fwrite("Select of ~p rows took: ~p milliseconds~n", [length(Rows), SelectTime/1000]),
+ 
     ok.
 
 appender() ->
     {ok, Db} = educkdb:open(":memory:"),
     {ok, Conn} = educkdb:connect(Db),
 
-    {ok, _, []} = q(Conn, "create table bench (a int, b int, c int, d int, e int, f int, g int, h int, i int, j int)"),
+    {ok, []} = q(Conn, "create table bench (a int, b int, c int, d int, e int, f int, g int, h int, i int, j int)"),
 
     Values = lists:seq(1, 10),
     Range = lists:seq(1, 100_000),
 
-    %% Insert 1_000_000 records
+    %% Insert 100_000 records
     {Time, _} = timer:tc(fun() ->
-                                 {ok, [], []} = q(Conn, "begin;"),
+                                 {ok, []} = educkdb:squery(Conn, "begin;"),
                                  {ok, Appender} = educkdb:appender_create(Conn, undefined, <<"bench">>),
                                  lists:foreach(fun(_) ->
                                                        [ ok = educkdb:append_int32(Appender, V) || V <- Values ],
@@ -39,17 +46,22 @@ appender() ->
                                                end,
                                                Range),
                                  educkdb:appender_flush(Appender),
-                                 {ok, [], []} = q(Conn, "commit;")
+                                 {ok, []} = educkdb:squery(Conn, "commit;")
                          end),
 
     io:fwrite("100_000 inserts took: ~p milliseconds~n", [Time/1000]),
 
     %% Select 100_000 
-    {SelectTime, {ok, _, Rows}} = timer:tc(fun() ->
-                                                   q(Conn, "select * from bench;")
-                                           end),
+    {SelectTime, Data} = timer:tc(fun() ->
+                                          {ok, R} = educkdb:query(Conn, "select * from bench"),
+                                          Chunks = educkdb:get_chunks(R),
+                                          [ educkdb:extract_chunk(C) || C <- Chunks ]
+                                  end),
     
-    io:fwrite("Select of ~p rows took: ~p milliseconds~n", [length(Rows), SelectTime/1000]),
+    NrChunks = length(Data),
+    NrRows = lists:sum([ begin [ #{ data := C } | _ ] = D, length(C) end || D <- Data ]),
+
+    io:fwrite("Select of ~p chunks and ~p rows took: ~p milliseconds~n", [NrChunks, NrRows, SelectTime/1000]),
 
     ok.
 
@@ -58,14 +70,14 @@ main() ->
     {ok, Db} = educkdb:open(":memory:"),
     {ok, Conn} = educkdb:connect(Db),
 
-    {ok, _, []} = q(Conn, "create table bench (a int, b int, c int, d int, e int, f int, g int, h int, i int, j int)"),
+    {ok, []} = q(Conn, "create table bench (a int, b int, c int, d int, e int, f int, g int, h int, i int, j int)"),
 
     Values = lists:seq(1, 10),
     Range = lists:seq(1, 100_000),
 
-    %% Insert 1_000_000 records
+    %% Insert 100_000 records
     {Time, _} = timer:tc(fun() ->
-                                 {ok, [], []} = q(Conn, "begin;"),
+                                 {ok, []} = q(Conn, "begin;"),
 
                                  {ok, Stmt} = educkdb:prepare(Conn, "insert into bench values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"),
                                  lists:foreach(fun(_) ->
@@ -74,30 +86,29 @@ main() ->
                                                end,
                                                Range),
 
-                                 {ok, [], []} = q(Conn, "commit;")
+                                 {ok, []} = q(Conn, "commit;")
                          end),
 
     io:fwrite("100_000 inserts took: ~p milliseconds~n", [Time/1000]),
 
     %% Select 100_000 
-    {SelectTime, {ok, _, Rows}} = timer:tc(fun() ->
-                                                   q(Conn, "select * from bench;")
-                                           end),
+    {SelectTime, Data} = timer:tc(fun() ->
+                                          {ok, R} = educkdb:query(Conn, "select * from bench"),
+                                          Chunks = educkdb:get_chunks(R),
+                                          [ educkdb:extract_chunk(C) || C <- Chunks ]
+                                  end),
     
-    io:fwrite("Select of ~p rows took: ~p milliseconds~n", [length(Rows), SelectTime/1000]),
+    NrChunks = length(Data),
+    NrRows = lists:sum([ begin [ #{ data := C } | _ ] = D, length(C) end || D <- Data ]),
+
+    io:fwrite("Select of ~p chunks and ~p rows took: ~p milliseconds~n", [NrChunks, NrRows, SelectTime/1000]),
+
+
 
     ok.
 
 
-q(Conn, Query) ->
-    case educkdb:query(Conn, Query) of
-        {ok, Result} -> educkdb:extract_result(Result);
-        {error, _}=E -> E
-    end.
+q(Conn, Query) -> educkdb:squery(Conn, Query).
 
-x(Stmt) ->
-    case educkdb:execute_prepared(Stmt) of
-        {ok, Result} -> educkdb:extract_result(Result);
-        {error, _}=E -> E
-    end.
+x(Stmt) -> educkdb:execute(Stmt). 
 
